47a798f9b1c726c2e1d4aaaafaa9b40224bbb06c
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2026-04-30 02:53:32 +0700

    Update app3625.py

--- app3625.py
+++ app3625.py
@@ -6,13 +6,13 @@ st.set_page_config(initial_sidebar_state="collapsed")
 
 # ===== XOÁ SIDEBAR CHUẨN =====
 st.markdown("""
-<style>
-        [data-testid="stSidebar"] {
+    <style>
+        [data-testid="stSidebarNav"] {
             display: none;
         }
     </style>
-  
-""", unsafe_allow_html=True)
+    """,
+    unsafe_allow_html=True,
 
 # ===== LOAD DATA =====
 @st.cache_data(ttl=60)
