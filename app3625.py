68cc3865654cd27da4c04c09caaa7f0f226c5da6
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2026-04-30 03:09:45 +0700

    Update app3625.py

--- app3625.py
+++ app3625.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="wide")
-
+st.set_page_config(page_title="FTD KPI SYSTEM", layout="wide", initial_sidebar_state="collapsed")
 # ===== XOÁ SIDEBAR CHUẨN =====
 st.markdown("""
 <style>
